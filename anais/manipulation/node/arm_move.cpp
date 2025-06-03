d9f57a66ce8ef6a08848f73d618961ec21eabf62
Pablo González Monfort <pablohalcoguepard@gmail.com>
2025-06-03 19:05:50 +0200

    avances

--- anais/manipulation/node/arm_move.cpp
+++ anais/manipulation/node/arm_move.cpp
@@ -1,150 +1,265 @@
-// src/arm_move.cpp
-
 #include <ros/ros.h>
 #include <moveit/move_group_interface/move_group_interface.h>
 #include <geometry_msgs/Pose.h>
+#include <std_msgs/String.h>
 #include <vector>
 
-int main(int argc, char** argv)
+///// Constants /////
+
+// Poses are defined with x, y, z coordinates and orientation in quaternion format (x, y, z, w).
+const float HOME_POSE[7] = {
+	-0.021,  // x
+	0.0,     // y
+	0.378,   // z
+	0.0,     // orientation.x
+	-0.541,  // orientation.y
+	0.0,     // orientation.z
+	0.841    // orientation.w
+};
+
+const float PICK_POSE[7] = {
+	-0.037,  // x
+	0.0,     // y
+	0.45,    // z
+	0.0,     // orientation.x
+	-0.667,  // orientation.y
+	0.0,     // orientation.z
+	0.745    // orientation.w
+};
+
+const float PLACE_POSE[7] = {
+	0.178,   // x
+	0.0,     // y
+	0.11,    // z
+	0.0,  // orientation.x
+	0.464,   // orientation.y
+	0.014,   // orientation.z
+	0.886    // orientation.w
+};
+
+const float CLOSED_GRIPPER_POSE[2] = {
+	0.0,
+	0.0
+};
+
+const float OPEN_GRIPPER_POSE[2] = {
+	0.0079,
+	0.0079
+};
+
+///// Global variables /////
+
+ros::NodeHandle nh;
+ros::Publisher orquestator_communication_publisher = nh.advertise<std_msgs::String>("/orquestator_manipulation", 1000);
+
+// Possible values for the orquestator_communication_msg.data:
+// "none" - No action required
+// "pick" - Activate pick function
+// "place" - Activate place function
+// "done_pick" - Notify that the pick action is done (no action required)
+// "done_place" - Notify that the place action is done (no action required)
+std_msgs::String orquestator_communication_msg;
+
+moveit::planning_interface::MoveGroupInterface move_group("arm");
+moveit::planning_interface::MoveGroupInterface gripper_group("gripper");
+
+
+///// Function declarations /////
+
+int move_arm(const float pose[7], int log_level = 1) {
+	// Set the target pose for the arm
+	// log indicates the log level:
+	// 		0 - Silent
+	// 		1 - Error
+	// 		2 - Info
+	// Returns 0 on success, 1 on planning failure, 2 on execution failure
+
+	geometry_msgs::Pose target_pose;
+	target_pose.position.x = pose[0];
+	target_pose.position.y = pose[1];
+	target_pose.position.z = pose[2];
+	target_pose.orientation.x = pose[3];
+	target_pose.orientation.y = pose[4];
+	target_pose.orientation.z = pose[5];
+	target_pose.orientation.w = pose[6];
+
+	move_group.setPoseTarget(target_pose);
+
+	moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+	moveit::core::MoveItErrorCode plan_success = move_group.plan(my_plan);
+	if (plan_success != moveit::core::MoveItErrorCode::SUCCESS) {
+		if (log_level > 0) {
+			ROS_ERROR_STREAM("Planning to target pose failed with error code: " << plan_success);
+		}
+		return 1;
+	}
+	moveit::core::MoveItErrorCode exe_success = move_group.execute(my_plan);
+	if (exe_success != moveit::core::MoveItErrorCode::SUCCESS) {
+		if (log_level > 0) {
+			ROS_ERROR_STREAM("Execution to target pose failed with error code: " << exe_success);
+		}
+		return 2;
+	}
+	if (log_level > 1) {
+		ROS_INFO("Execution to target pose succeeded.");
+	}
+	return 0;
+}
+
+int move_gripper(const float gripper_pose[2], int log_level = 1) {
+	// Set the target pose for the gripper
+	// log indicates the log level:
+	// 		0 - Silent
+	// 		1 - Error
+	// 		2 - Info
+	// Returns 0 on success, 1 on planning failure, 2 on execution failure
+
+	std::vector<double> gripper_joint_values = {
+		gripper_pose[0],
+		gripper_pose[1]
+	};
+
+	gripper_group.setJointValueTarget(gripper_joint_values);
+
+	moveit::planning_interface::MoveGroupInterface::Plan gripper_plan;
+	moveit::core::MoveItErrorCode gripper_plan_success = gripper_group.plan(gripper_plan);
+	if (gripper_plan_success != moveit::core::MoveItErrorCode::SUCCESS) {
+		if (log_level > 0) {
+			ROS_ERROR_STREAM("Planning to gripper pose failed with error code: " << gripper_plan_success);
+		}
+		return 1;
+	}
+	moveit::core::MoveItErrorCode gripper_exe_success = gripper_group.execute(gripper_plan);
+	if (gripper_exe_success != moveit::core::MoveItErrorCode::SUCCESS) {
+		if (log_level > 0) {
+			ROS_ERROR_STREAM("Execution to gripper pose failed with error code: " << gripper_exe_success);
+		}
+		return 2;
+	}
+	if (log_level > 1) {
+		ROS_INFO("Execution to gripper pose succeeded.");
+	}
+	return 0;
+}
+
+bool pick_ball_action() {
+	// Move to pick pose
+	int result = move_arm(PICK_POSE);
+	if (result != 0) {
+		return false; // Return false if there was an error
+	}
+	
+	// Open the gripper
+	result = move_gripper(OPEN_GRIPPER_POSE);
+	if (result != 0) {
+		return false; // Return false if there was an error
+	}
+
+	// Wait 5 seconds
+	ros::Duration(5.0).sleep();
+
+	// Close the gripper
+	result = move_gripper(CLOSED_GRIPPER_POSE);
+	if (result != 0) {
+		return false; // Return false if there was an error
+	}
+
+	// Move to home pose
+	result = move_arm(HOME_POSE);
+	if (result != 0) {
+		return false; // Return false if there was an error
+	}
+
+	return true; // Return true if the action was successful
+}
+
+bool place_ball_action() {
+	// Move to place pose
+	int result = move_arm(PLACE_POSE);
+	if (result != 0) {
+		return false; // Return false if there was an error
+	}
+
+	// Open the gripper
+	result = move_gripper(OPEN_GRIPPER_POSE);
+	if (result != 0) {
+		return false; // Return false if there was an error
+	}
+
+	// Move to home pose
+	result = move_arm(HOME_POSE);
+	if (result != 0) {
+		return false; // Return false if there was an error
+	}
+
+	// Close the gripper
+	result = move_gripper(CLOSED_GRIPPER_POSE);
+	if (result != 0) {
+		return false; // Return false if there was an error
+	}
+
+	return true; // Return true if the action was successful
+}
+
+void comunicationCallback(const std_msgs::String::ConstPtr &msg)
 {
-  ros::init(argc, argv, "tb3_arm_move_example");
-  ros::NodeHandle nh;
-  ros::AsyncSpinner spinner(1);
-  spinner.start();
+	if (msg->data == "pick") {
+		orquestator_communication_msg.data = "none"; // Reset the message to avoid repeated actions
+		orquestator_communication_publisher.publish(orquestator_communication_msg); // Publish the reset message
 
-  //
-  // 1) Create a MoveGroupInterface for the manipulator group
-  //
-  //    In turtlebot3_manipulation_moveit_config, the group name is usually "arm",
-  //    but you can confirm by inspecting:
-  //      rosparam get /move_group/planning_groups
-  //    or by opening the SRDF under your config package.
-  //
+		ROS_INFO("Initiating pick action...");
+		bool result = pick_ball_action();
+		if (result) {
+			ROS_INFO("Pick action completed successfully.");
+		} else {
+			ROS_ERROR("Pick action failed.");
+		}
 
-  moveit::planning_interface::MoveGroupInterface move_group("arm");
-  moveit::planning_interface::MoveGroupInterface gripper_group("gripper");
+		orquestator_communication_msg.data = "done_pick";
+		orquestator_communication_publisher.publish(orquestator_communication_msg); // Notify that the pick action is done
 
-  ros::Duration(1.0).sleep(); // Sleep for 1 second to allow the groups to initialize
+	} else if (msg->data == "place") {
+		orquestator_communication_msg.data = "none"; // Reset the message to avoid repeated actions
+		orquestator_communication_publisher.publish(orquestator_communication_msg); // Publish the reset message
 
-  move_group.setMaxVelocityScalingFactor(0.1); // Set the velocity scaling factor
-  move_group.setMaxAccelerationScalingFactor(0.1); // Set the acceleration scaling factor
-  move_group.setGoalPositionTolerance(0.03); // Set position tolerance
-  move_group.setGoalOrientationTolerance(0.1); // Set orientation tolerance
-  move_group.setPlanningTime(10.0);
-  move_group.setNumPlanningAttempts(2);
+		ROS_INFO("Initiating place action...");
+		bool result = place_ball_action();
+		if (result) {
+			ROS_INFO("Place action completed successfully.");
+		} else {
+			ROS_ERROR("Place action failed.");
+		}
 
+		orquestator_communication_msg.data = "done_place";
+		orquestator_communication_publisher.publish(orquestator_communication_msg); // Notify that the place action is done
+	}
+}
 
-  // (Optional) If you want to see what controllers are attached:
-  // move_group.getJointModelGroup("arm")->getActiveJoints();
+///// Main function /////
 
-  //
-  // 2) Read current end‐effector pose (initial pose)
-  //
-  geometry_msgs::PoseStamped current_pose_stamped = move_group.getCurrentPose();
-  ROS_INFO_STREAM_NAMED("initial_pose", 
-    "Initial pose: \n"
-    << current_pose_stamped.pose.position.x << ", "
-    << current_pose_stamped.pose.position.y << ", "
-    << current_pose_stamped.pose.position.z << ", "
-    << current_pose_stamped.pose.orientation.x << ", "
-    << current_pose_stamped.pose.orientation.y << ", "
-    << current_pose_stamped.pose.orientation.z << ", "
-    << current_pose_stamped.pose.orientation.w);
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "RoboticArmMovement");
+	
+	// ros::AsyncSpinner spinner(1);
+	// spinner.start();
 
-  //
-  // 3) Define your goal pose
-  //
-  geometry_msgs::Pose target_pose;
-  // For example: shift +0.1 m in x, keep the same orientation:
-  //up -0.0367087, 0.00111089, 0.545741, 0.00855517, -0.666897, 0.00955762, 0.74504
-  //down 0.178152, 0.00787757, 0.10994, -0.00708296, 0.464333, 0.0135079, 0.885529
-  target_pose.position.x = -0.037;
-  target_pose.position.y = 0.0;
-  target_pose.position.z = 0.45;
-  target_pose.orientation.x = 0.0;
-  target_pose.orientation.y = -0.667;
-  target_pose.orientation.z = 0.0;
-  target_pose.orientation.w = 0.745;
+	ros::Duration(1.0).sleep(); // Sleep for 1 second to allow the groups to initialize
 
-  move_group.setPoseTarget(target_pose);
+	move_group.setGoalPositionTolerance(0.03);
+	move_group.setGoalOrientationTolerance(0.1);
+	move_group.setPlanningTime(10.0);
+	move_group.setNumPlanningAttempts(2);
 
-  //
-  // 4) Plan to that pose
-  //
-  moveit::planning_interface::MoveGroupInterface::Plan my_plan;
-  bool success = (move_group.plan(my_plan) == moveit::core::MoveItErrorCode::SUCCESS);
-  if (!success)
-  {
-    ROS_ERROR("Failed to plan to target pose");
-    // ros::shutdown();
-    // return 1;
-  } else {
-    ROS_INFO("Planning to goal pose succeeded.");
-  }
+	ros::Subscriber orquestator_communication_subscriber = nh.subscribe("/orquestator_manipulation", 1000, comunicationCallback);
 
-  //
-  // 5) Execute the plan
-  //
-  moveit::core::MoveItErrorCode exe_success = move_group.execute(my_plan);
-  if (exe_success != moveit::core::MoveItErrorCode::SUCCESS)
-  {
-    // Print the error code for debugging
-    ROS_ERROR_STREAM("Execution to goal pose failed with error code: " << exe_success);
-    // ros::shutdown();
-    // return 1;
-  } else {
-    ROS_INFO("Execution to goal pose succeeded.");
-  }
+	ros::Rate loop_rate(1); // 1 Hz
 
-  // Read current gripper joint values
-  std::vector<double> gripper_joint_values = gripper_group.getCurrentJointValues();
-  ROS_INFO_STREAM_NAMED("gripper_joint_values", 
-    "Current gripper joint values: "
-    << gripper_joint_values[0] << ", "
-    << gripper_joint_values[1]);
-  // Set gripper joint values to close the gripper
-  gripper_joint_values[0] = 0.15; // Close the gripper
-  gripper_joint_values[1] = 0.15; // Close the gripper
-  gripper_group.setJointValueTarget(gripper_joint_values);
-  // Plan to close the gripper
-  moveit::planning_interface::MoveGroupInterface::Plan gripper_plan;
-  bool gripper_success = (gripper_group.plan(gripper_plan) == moveit::core::MoveItErrorCode::SUCCESS);
-  if (!gripper_success)
-  {
-    ROS_ERROR("Failed to plan to close gripper");
-    // ros::shutdown();
-    // return 1;
-  } else {
-    ROS_INFO("Planning to close gripper succeeded.");
-  }
-  // Execute the gripper plan
-  moveit::core::MoveItErrorCode gripper_exe_success = gripper_group.execute(gripper_plan);
-  if (gripper_exe_success != moveit::core::MoveItErrorCode::SUCCESS)
-  {
-    // Print the error code for debugging
-    ROS_ERROR_STREAM("Execution to close gripper failed with error code: " << gripper_exe_success);
-    // ros::shutdown();
-    // return 1;
-  } else {
-    ROS_INFO("Execution to close gripper succeeded.");
-  }
+	while (ros::ok()) {
+		ros::spinOnce(); // Process incoming messages
+		// orquestator_communication_publisher.publish(orquestator_communication_msg);
+		loop_rate.sleep();
+	}
 
-
-  while (ros::ok())
-  {
-    geometry_msgs::PoseStamped current_pose_stamped = move_group.getCurrentPose();
-    ROS_INFO_STREAM_NAMED("current_pose", 
-      "Current pose: \n"
-      << current_pose_stamped.pose.position.x << ", "
-      << current_pose_stamped.pose.position.y << ", "
-      << current_pose_stamped.pose.position.z << ", "
-      << current_pose_stamped.pose.orientation.x << ", "
-      << current_pose_stamped.pose.orientation.y << ", "
-      << current_pose_stamped.pose.orientation.z << ", "
-      << current_pose_stamped.pose.orientation.w);
-    ros::Duration(1.5).sleep(); // Sleep for 1.5 seconds to avoid flooding the console
-  }
-
-  ros::shutdown();
-  return 0;
+	return 0;
 }
