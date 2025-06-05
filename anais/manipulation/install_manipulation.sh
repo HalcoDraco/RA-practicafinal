1def6feb4017de5c115db8cf7ac788c44a1038e5
HalcoDraco <pablohalcoguepard@gmail.com>
2025-06-05 13:18:04 +0200

    install

--- anais/manipulation/install_manipulation.sh
+++ anais/manipulation/install_manipulation.sh
@@ -13,3 +13,13 @@ sudo apt clean
 sudo apt update --allow-insecure-repositories
 sudo apt install -y --allow-unauthenticated ros-noetic-ros-control* ros-noetic-control* ros-noetic-moveit* ros-noetic-dwa-local-planner
 # catkin make (cm)
+
+
+# si el brazo no se yergue, ejecutar lo siguiente dentro del robot (con ssh):
+export OPENCR_PORT=/dev/ttyACM0
+export OPENCR_MODEL=om_with_tb3_noetic
+rm -rf ./opencr_update.tar.bz2
+wget https://github.com/ROBOTIS-GIT/OpenCR-Binaries/raw/master/turtlebot3/ROS1/latest/opencr_update.tar.bz2
+tar -xvf opencr_update.tar.bz2
+cd ./opencr_update
+./update.sh $OPENCR_PORT $OPENCR_MODEL.opencr
