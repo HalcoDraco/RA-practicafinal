701861cca8113982efa0615ecaa243d4e517b622
HalcoDraco <pablohalcoguepard@gmail.com>
2025-06-05 10:42:49 +0200

    install change

--- anais/manipulation/install_manipulation.sh
+++ anais/manipulation/install_manipulation.sh
@@ -4,3 +4,12 @@ git clone -b noetic https://github.com/ROBOTIS-GIT/turtlebot3_manipulation_simul
 git clone -b noetic https://github.com/ROBOTIS-GIT/open_manipulator_dependencies.git
 sudo apt install ros-noetic-ros-control* ros-noetic-control* ros-noetic-moveit* ros-noetic-dwa-local-planner
 cd ~/catkin_ws && catkin_make
+
+
+# if sudo apt install ros-noetic-ros-control* ros-noetic-control* ros-noetic-moveit* ros-noetic-dwa-local-planner
+# fails, try the following:
+sudo rm -rf /var/lib/apt/lists/*
+sudo apt clean
+sudo apt update --allow-insecure-repositories
+sudo apt install -y --allow-unauthenticated ros-noetic-ros-control* ros-noetic-control* ros-noetic-moveit* ros-noetic-dwa-local-planner
+# catkin make (cm)
